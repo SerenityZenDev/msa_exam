d0a157f8998f1dc3c4a28b4cb4fbd6685782b1a3
SerenityZenDev <tjrqo2000@gmail.com>
2024-08-06 14:06:38 +0900

    :sparkles: Add Product Entity

---
+++ com.sparta.msa_exam.product/src/main/java/com/sparta/msa_exam/product/product/Product.java
@@ -0,0 +1,32 @@
+package com.sparta.msa_exam.product.product;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long product_id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private int supply_price;
+
+    @Builder
+    public Product(String name, int supply_price) {
+        this.name = name;
+        this.supply_price = supply_price;
+    }
+}
