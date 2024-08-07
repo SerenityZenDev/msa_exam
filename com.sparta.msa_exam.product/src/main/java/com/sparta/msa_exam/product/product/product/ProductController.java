3da142027503134ac75a065f8e7aa13fa9d3d0e8
SerenityZenDev <tjrqo2000@gmail.com>
2024-08-07 15:27:13 +0900

    :sparkles: Add Port (Get Product Response Header)

--- com.sparta.msa_exam.product/src/main/java/com/sparta/msa_exam/product/product/product/ProductController.java
+++ com.sparta.msa_exam.product/src/main/java/com/sparta/msa_exam/product/product/product/ProductController.java
@@ -1,7 +1,9 @@
 package com.sparta.msa_exam.product.product.product;
 
+import jakarta.servlet.http.HttpServletResponse;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,8 +16,12 @@ public class ProductController {
 
     private final ProductService productService;
 
+    @Value("${server.port}")
+    private String serverPort;
+
     @GetMapping("/products")
-    public List<Product> getProducts() {
+    public List<Product> getProducts(HttpServletResponse response) {
+        response.addHeader("Server-Port", serverPort);
         return productService.getAllProducts();
     }
 
